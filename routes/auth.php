68d22d75af665e5c97d0987e7b44a913cebafc42
Devsam_ <sabillyamri1@gmail.com>
2025-12-24 09:15:49 +0700

    fix : add kernel

--- routes/auth.php
+++ routes/auth.php
@@ -4,6 +4,6 @@ use App\Http\Controllers\Auth\AuthenticatedController;
 use Illuminate\Support\Facades\Route;
 
 // Route::post('/register', [AuthController::class, 'register'])->name('auth.register')->middleware('guest');
-Route::get('/', [AuthenticatedController::class, 'login'])->name('auth.login');
-Route::post('/login', [AuthenticatedController::class, 'authenticate'])->name('auth.authenticate');
+Route::get('/', [AuthenticatedController::class, 'login'])->name('auth.login')->middleware('guest');
+Route::post('/login', [AuthenticatedController::class, 'authenticate'])->name('auth.authenticate')->middleware('guest');
 Route::post('/logout', [AuthenticatedController::class, 'logout'])->name('auth.logout')->middleware('auth');
